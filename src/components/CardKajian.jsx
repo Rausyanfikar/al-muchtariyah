50b6788dce66be78b575350879baef37493160a1
rausyanfikar <rauzyanfikar@gmail.com>
2022-09-12 18:29:36 +0700

    responsive hp

--- src/components/CardKajian.jsx
+++ src/components/CardKajian.jsx
@@ -14,28 +14,28 @@ const CardKajian = () => {
 
         {/* kegiatan kajian*/}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 ">
-          <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={ibu} alt="bagiminyak" />
+          <div className="shadow-md shadow-[#040c16]  w-full hover:scale-110 duration-500 ">
+            <img className="w-full sm:h-72 h-52 mx-auto " src={ibu} alt="bagiminyak" />
             <p className="my-4">Kegitan Shodaqoh minyak untuk masyarakat sekita</p>
           </div>
           <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={kesehatan} alt="bagiminyak" />
+            <img className="w-full sm:h-72 h-52 mx-auto " src={kesehatan} alt="bagiminyak" />
             <p className="my-4">Kegitan pengajian rutin ibu -ibu</p>
           </div>
           <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={kajian} alt="bagiminyak" />
+            <img className="w-full sm:h-72 h-52 mx-auto " src={kajian} alt="bagiminyak" />
             <p className="my-4">Kegitan pengajian rutin ibu -ibu</p>
           </div>
           <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={adzan} alt="bagiminyak" />
+            <img className="w-full sm:h-72 h-52 mx-auto " src={adzan} alt="bagiminyak" />
             <p className="my-4">Kegitan pengajian rutin ibu -ibu</p>
           </div>
           <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={kajian} alt="bagiminyak" />
+            <img className="w-full sm:h-72 h-52 mx-auto " src={kajian} alt="bagiminyak" />
             <p className="my-4">Kegitan pengajian rutin ibu -ibu</p>
           </div>
           <div className="shadow-md shadow-[#040c16] w-full hover:scale-110 duration-500 ">
-            <img className="w-full h-72 mx-auto " src={kajian} alt="bagiminyak" />
+            <img className="w-full sm:h-72 h-52 mx-auto " src={kajian} alt="bagiminyak" />
             <p className="my-4">Kegitan pengajian rutin ibu -ibu</p>
           </div>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import '../styles/App.css';
 
 const Hero = () => {
   return (
-    <div className="hero relative  background flex flex-col items-center justify-center border mb-5 ">
+    <div className="hero relative   background flex flex-col items-center justify-center border mb-5 ">
       <div className="hero-overlay bg-opacity-40"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md ">
-          <h1 className="mb-5 text-5xl  text-white font-bold">AL - MUCHTARIYAH</h1>
+          <h1 className="mb-5 text-xl sm:text-5xl  text-white font-bold">AL - MUCHTARIYAH</h1>
           <button className="btn btn-primary bg-[#1cac85] p-3 text-white rounded-lg hover:bg-[#179675]">Tentang Al-Muchtariyah</button>
         </div>
       </div>
